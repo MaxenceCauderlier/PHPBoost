2d921a13db3c2da48294275d058e48e34baf1107
Max Koder <mx.koder@gmail.com>
2024-09-10 15:28:56 +0200

    [MAJ] ORM

--- kernel/framework/mvc/model/ORMModel.class.php
+++ kernel/framework/mvc/model/ORMModel.class.php
@@ -9,6 +9,8 @@ abstract class ORMModel
 
     protected $attributes = [];
 
+    protected $own_fields = [];
+
     abstract public static function get_table_name(): string;
 
     /**
@@ -16,7 +18,8 @@ abstract class ORMModel
      *
      * @return void
      */
-    public function __construct($attributes = []) {
+    public function __construct($attributes = [])
+    {
         $this->attributes = $attributes;
     }
 
@@ -29,7 +32,7 @@ abstract class ORMModel
     {
         return PersistenceContext::get_querier();
     }
-    
+
     /**
      * Adds a belongs_to relationship to the model.
      *
@@ -49,7 +52,7 @@ abstract class ORMModel
         ];
         return $this;
     }
-    
+
     /**
      * Adds a has_many relationship to the model.
      *
@@ -73,25 +76,26 @@ abstract class ORMModel
         ];
         return $this;
     }
-    
+
     /**
      * Adds a has_many_through relationship to the model.
      *
      * @param string $related_model The class name of the related model.
      * @param string $through_model The class name of the through model.
+     * @param string $items_name The name of the property that will contain the items.
      * @param string $first_key The key on the through model that references the current model.
      * @param string $second_key The key on the through model that references the related model.
      * @param string|null $local_key The local key column name. Defaults to the primary key.
      * @param string|null $second_local_key The local key column name of the related model. Defaults to the primary key.
      * @return self
      */
-    public function has_many_through($related_model, $through_model, $first_key, $second_key, $local_key = false, $second_local_key = false): self
+    public function has_many_through(string $related_model, string $through_model, string $items_name, $first_key, $second_key, $local_key = false, $second_local_key = false): self
     {
         if ($local_key === false) {
             $local_key = static::$primary_key;
         }
         if ($second_local_key === false) {
-            $second_local_key = static::$primary_key;
+            $second_local_key = $related_model::$primary_key;
         }
         $this->relations[] = [
             'type' => 'has_many_through',
@@ -100,7 +104,8 @@ abstract class ORMModel
             'first_key' => $first_key,
             'second_key' => $second_key,
             'local_key' => $local_key,
-            'second_local_key' => $second_local_key
+            'second_local_key' => $second_local_key,
+            'items_name' => $items_name
         ];
         return $this;
     }
@@ -113,7 +118,7 @@ abstract class ORMModel
      * @param string|null $local_key The local key column name. Defaults to the primary key.
      * @return self
      */
-    public function has_one($related_model, $foreign_key, $local_key = false): self
+    public function has_one(string $related_model, string $item_name, string $foreign_key, $local_key = false): self
     {
         if ($local_key === false) {
             $local_key = static::$primary_key;
@@ -122,7 +127,8 @@ abstract class ORMModel
             'type' => 'has_one',
             'model' => $related_model,
             'foreign_key' => $foreign_key,
-            'local_key' => $local_key
+            'local_key' => $local_key,
+            'item_name' => $item_name
         ];
         return $this;
     }
@@ -138,15 +144,16 @@ abstract class ORMModel
      *
      * @return array[ORMModel] An array of objects of the current class.
      */
-    public function get():array
+    public function get(): array
     {
-        $this->query = (stripos($this->query, 'SELECT') === false) ?  "SELECT * FROM " . static::get_table_name() . $this->query : $this->query;
+        $this->query = (stripos($this->query, 'SELECT') === false) ? "SELECT * FROM " . static::get_table_name() . $this->query : $this->query;
         $results = self::get_querier()->select($this->query, $this->bindings);
         $class_name = get_called_class();
         $objects = [];
         while ($row = $results->fetch()) {
             $obj = new $class_name();
             foreach ($row as $key => $value) {
+                $obj->own_fields[$key] = $key;
                 $obj->attributes[$key] = $value;
             }
             $objects[] = $obj;
@@ -248,15 +255,21 @@ abstract class ORMModel
     // Méthode pour charger une relation has_one avec eager loading
     protected function load_has_one_relation(&$objects, $relation)
     {
+        /**
+         * @var ORMModel $related_model
+         */
         $related_model = new $relation['model']();
         $local_keys = array_unique(array_map(function ($result) use ($relation) {
-            return $result->{$relation['local_key']};
+            return $result->attributes[$relation['local_key']];
         }, $objects));
 
         if (empty($local_keys)) {
             return;
         }
 
+        /**
+         * @var array[ORMModel] $related_records
+         */
         $related_records = $related_model->where_in($relation['foreign_key'], $local_keys)->get();
 
         $related_map = [];
@@ -266,8 +279,7 @@ abstract class ORMModel
 
         foreach ($objects as $result) {
             $local_key_value = $result->{$relation['local_key']};
-            $relation_name = strtolower($relation['model']);
-            $result->$relation_name = $related_map[$local_key_value] ?? null;
+            $result->attributes[$relation['item_name']] = $related_map[$local_key_value] ?? null;
         }
     }
 
@@ -276,7 +288,7 @@ abstract class ORMModel
     {
         $through_model = new $relation['through']();
         $local_keys = array_unique(array_map(function ($result) use ($relation) {
-            return $result->{$relation['local_key']};
+            return $result->attributes[$relation['local_key']];
         }, $objects));
 
         if (empty($local_keys)) {
@@ -308,44 +320,57 @@ abstract class ORMModel
 
         foreach ($objects as $result) {
             $local_key_value = $result->{$relation['local_key']};
-            $relation_name = strtolower($relation['model']) . 's';
-            $result->$relation_name = [];
+            $result->attributes[$relation['items_name']] = [];
 
             if (isset($through_map[$local_key_value])) {
                 foreach ($through_map[$local_key_value] as $second_key) {
                     if (isset($related_map[$second_key])) {
-                        $result->{$relation_name}[] = $related_map[$second_key];
+                        $result->attributes[$relation['items_name']][] = $related_map[$second_key];
                     }
                 }
             }
         }
     }
 
-    
+
 
     // Méthode pour récupérer un seul résultat
-    public function first()
+    public function first(): ?self
     {
-        $this->query .= " LIMIT 1";
+        $this->limit(1);
         $results = $this->get();
         return !empty($results) ? $results[0] : null;
     }
 
-
-    public function find($id): ?ORMModel
+    public function last(): ?self
     {
-        return $this->where(static::$primary_key, '=', $id)->first();
+        $this->limit(1);
+        $results = $this->get();
+        return !empty($results) ? $results[count($results) - 1] : null;
+    }
+    
+
+
+    public function find($id): ?self
+    {
+        return $this->find_by(static::$primary_key, $id);
+    }
+
+    public function find_by(string $field, $value): ?self
+    {
+        return $this->where($field, '=', $value)->first();
     }
 
     public function count()
     {
         $this->query = "SELECT COUNT(*) FROM " . static::get_table_name() . $this->query;
-        $res = (int)$this->first()->attributes['COUNT(*)'];
+        $res = (int) $this->first()->attributes['COUNT(*)'];
         $this->reset();
         return $res;
     }
 
-    public function save() {
+    public function save()
+    {
         if (isset($this->attributes[static::$primary_key])) {
             return $this->update();
         } else {
@@ -354,105 +379,156 @@ abstract class ORMModel
     }
 
     // Méthode pour insérer un nouvel enregistrement
-    protected function insert() {
-        $result = self::get_querier()->insert(static::get_table_name(), $this->attributes);
+    protected function insert()
+    {
+        $result = self::get_querier()->insert(static::get_table_name(), $this->get_own_fields());
         $this->attributes[static::$primary_key] = $result->get_last_inserted_id();
-        
         return $this;
     }
 
     // Méthode pour mettre à jour un enregistrement
-    protected function update() {
-        self::get_querier()->update(static::get_table_name(), $this->attributes, 
-            'WHERE ' . static::$primary_key . '=:id', ['id' => $this->attributes[static::$primary_key]]);
-
+    protected function update()
+    {
+        self::get_querier()->update(
+            static::get_table_name(),
+            $this->get_own_fields(),
+            'WHERE ' . static::$primary_key . '=:id',
+            ['id' => $this->attributes[static::$primary_key]]
+        );
         return $this;
     }
 
     public function delete()
     {
-        if (isset($this->attributes[static::$primary_key])) 
-        {
+        if (isset($this->attributes[static::$primary_key])) {
             self::get_querier()->delete(static::get_table_name(), 'WHERE ' . static::$primary_key . '=:id', ['id' => $this->attributes[static::$primary_key]]);
             $this->reset();
             return true;
         }
-
         return false;
     }
 
-    // Méthode pour récupérer un enregistrement avec une condition
-    public function where($field, $operator, $value)
+    public function get_own_fields():array
     {
-        $this->query .= (stripos($this->query, 'WHERE') === false) ? " WHERE " : " AND ";
+        $attr = [];
+        foreach ($this->own_fields as $key) {
+            $attr[$key] = $this->attributes[$key];
+        }
+        return $attr;
+    }
+
+    /**
+     * Adds a WHERE clause to the query
+     *
+     * @param string $field The field to filter on
+     * @param string $operator The operator to use for the filter
+     * @param mixed $value The value to filter on
+     *
+     * @return ORMModel The current instance
+     */
+    public function where(string $field, string $operator, $value):self
+    {
+        if (substr($this->query, -1) !== '(')
+        {
+            // Open parenthesis
+            $this->query .= (stripos($this->query, 'WHERE') === false) ? " WHERE " : " AND ";
+        }
         $this->query .= "$field $operator :$field";
         $this->bindings[$field] = $value;
         return $this;
     }
 
-    // Méthode pour récupérer les enregistrements avec une condition IN
-    public function where_in($field, array $values)
+    /**
+     * Adds a WHERE IN clause to the query
+     *
+     * @param string $field The field to filter on
+     * @param array $values The values to filter on
+     *
+     * @return ORMModel The current instance
+     */
+    public function where_in(string $field, array $values):self
     {
         $placeholders = implode(',', $values);
-        $this->query .= (stripos($this->query, 'WHERE') === false) ? " WHERE " : " AND ";
+        if (substr($this->query, -1) !== '(')
+        {
+            // Open parenthesis
+            $this->query .= (stripos($this->query, 'WHERE') === false) ? " WHERE " : " AND ";
+        }
         $this->query .= "$field IN ($placeholders)";
         return $this;
     }
 
-    public function or_where($field, $operator, $value) {
+    public function or_where(string $field, string $operator, $value):self
+    {
         $this->query .= " OR $field $operator :$field";
         $this->bindings[$field] = $value;
         return $this;
     }
 
+    public function or_where_in(string $field, array $values):self
+    {
+        $placeholders = implode(',', $values);
+        $this->query .= " OR $field IN ($placeholders)";
+        return $this;
+    }
+
     // Méthode pour débuter un groupe de conditions (parenthèses ouvrantes)
-    public function group_start() {
+    public function group_start():self
+    {
         $this->query .= (stripos($this->query, 'WHERE') === false) ? " WHERE (" : " AND (";
         return $this;
     }
 
     // Méthode pour débuter un groupe de conditions avec un OR (parenthèses ouvrantes)
-    public function or_group_start() {
+    public function or_group_start():self
+    {
         $this->query .= " OR (";
         return $this;
     }
 
     // Méthode pour terminer un groupe de conditions (parenthèses fermantes)
-    public function group_end() {
+    public function group_end():self
+    {
         $this->query .= ")";
         return $this;
     }
 
-    public function order_by($field, $direction = 'ASC') {
+    public function order_by(string $field, $direction = 'ASC'):self
+    {
         $this->query .= " ORDER BY $field $direction";
         return $this;
     }
 
-    public function join($table, $first, $operator, $second, $type = 'INNER') {
+    public function join($table, $first, $operator, $second, $type = 'INNER')
+    {
         $this->query .= " $type JOIN $table ON $first $operator $second";
         return $this;
     }
 
-    public function group_by($field) {
+    public function group_by($field)
+    {
         $this->query .= " GROUP BY $field";
         return $this;
     }
 
     // Méthode pour ajouter une condition HAVING
-    public function having($field, $operator, $value) {
+    public function having($field, $operator, $value)
+    {
         $this->query .= " HAVING $field $operator :$field";
         $this->bindings[$field] = $value;
         return $this;
     }
 
     // Méthode pour ajouter une limite LIMIT
-    public function limit($number) {
+    public function limit($number)
+    {
         $this->query .= " LIMIT $number";
         return $this;
     }
 
     // Méthode pour ajouter un offset OFFSET
-    public function offset($number) {
+    public function offset($number)
+    {
         $this->query .= " OFFSET $number";
         return $this;
     }
@@ -461,16 +537,30 @@ abstract class ORMModel
     {
         $this->query = '';
         $this->bindings = [];
+        $this->relations = [];
     }
 
     public function __set($name, $value)
     {
-        $this->attributes[$name] = $value;
+        if (method_exists($this, "set_$name")) 
+        {
+            $this->{"set_$name"}($value);
+        } else 
+        {
+            $this->attributes[$name] = $value;
+        }
+
     }
 
     public function __get($name)
     {
+        if (method_exists($this, "get_$name"))
+        {
+            return $this->{"get_$name"}();
+        }
         return $this->attributes[$name] ?? null;
     }
+
+
 
 }
